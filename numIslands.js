707fd5359e3e5dce067b6ec2f5e85753b2b5e38f
Manel-Oulds <oulds.manel@gmail.com>
2023-10-13 11:45:19 -0400

    Added case the rows and columns greater then length

--- numIslands.js
+++ numIslands.js
@@ -6,21 +6,26 @@ function numIslands(grid) {
   let visited = [];
   let count = 0;
 
-  function DFS(ele_idx){
-        
-    let i,j = ele_idx; // i = 0 ; j =0  // i= -1 , j = 0
-    if (grid[i,j]===0 || i < 0 ||  j < 0  || visited.has(ele_idx)){
-        return null; 
+  function DFS(ele_idx) {
+    let i,
+      j = ele_idx; // i = 0 ; j =0  // i= -1 , j = 0
+    if (
+      grid[(i, j)] === 0 ||
+      i < 0 ||
+      i === grid.length ||
+      j < 0 ||
+      j === grid[0].length ||
+      visited.has(ele_idx)
+    ) {
+      return null;
     }
     // if(!visited.has(ele_idx)){
-        visited.push(ele_idx); // visited = [[0,0], [0,1]]
-        DFS([i-1,j]);    //DFS(-1,0) 
-        DFS([i+1,j]);
-        DFS([i,j-1]);
-        DFS([i,j+1]);   
-        // visited.push(ele_idx); // visited = [[0,0]]
+    visited.push(ele_idx); // visited = [[0,0], [0,1]]
+    DFS([i - 1, j]); //DFS(-1,0)
+    DFS([i + 1, j]);
+    DFS([i, j - 1]);
+    DFS([i, j + 1]);
+    // visited.push(ele_idx); // visited = [[0,0]]
     // }
-    
-}
-
+  }
 }
