41533bb75223f5afbc73e9a693f83a2507f4944f
Manel-Oulds <oulds.manel@gmail.com>
2023-10-13 11:43:03 -0400

    DFS working

--- numIslands.js
+++ numIslands.js
@@ -5,4 +5,18 @@ function numIslands(grid) {
 
   let visited = [];
   let count = 0;
+
+  function DFS(ele_idx) {
+    let i,
+      j = ele_idx; // i = 0 ; j =0  // i= -1 , j = 0
+
+    // if(!visited.has(ele_idx)){
+    visited.push(ele_idx); // visited = [[0,0], [0,1]]
+    DFS([i - 1, j]); //DFS(-1,0)
+    DFS([i + 1, j]);
+    DFS([i, j - 1]);
+    DFS([i, j + 1]);
+    // visited.push(ele_idx); // visited = [[0,0]]
+    // }
+  }
 }
