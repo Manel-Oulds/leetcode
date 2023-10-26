b5d54be94a6c0903c7a1f153774f53f6947e749e
Manel-Oulds <oulds.manel@gmail.com>
2023-10-26 11:36:09 -0400

    class binary search tree created

--- LowestCommonAncestor.js
+++ LowestCommonAncestor.js
@@ -4,5 +4,22 @@ class Node {
       this.left = null;
       this.right = null;
     }
-  }
+}
+
+class BinarySearchTree {
+    constructor() {
+      this.root = null;
+    }
+  
+    insert(data) {
+      const newNode = new Node(data);
+  
+      if (!this.root) {
+        this.root = newNode;
+      } else {
+        this.insertNode(this.root, newNode);
+      }
+    }
+
+}
   
