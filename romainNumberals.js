52b3f79649df74d736fc42d2df150f63ef223496
Manel-Oulds <oulds.manel@gmail.com>
2023-11-03 12:08:02 -0400

    running an exemple

--- romainNumberals.js
+++ romainNumberals.js
@@ -16,8 +16,7 @@
 
  */
 
- var romanToInt = function(s) {
-    let counter = 0;
+ var romanToInt = function(s) {// s = "MCM"  s ="III"
     let symbolValue = {
         "I" : 1,
         "V" : 5,
@@ -28,22 +27,24 @@
         "M" : 1000,
 
     };
-    let j =0;
-    while (j < s.length-1){ 
-        if (symbolValue[s[j]] < symbolValue[s[j+1]]){  
+
+    let j = 0;  // j =0
+    let counter = 0; // counter =0
+    while (j < s.length-1){ // 0< 2  // j < 2  ; j === length-1
+        if (symbolValue[s[j]] < symbolValue[s[j+1]]){  // 1 === 1 false
             counter+= symbolValue[s[j+1]] - symbolValue[s[j]]; 
             j+=2; 
         } 
         else{
-            counter+= symbolValue[s[j]];   
-            j++;    
+            counter+= symbolValue[s[j]];   // counter = 1 ; counter = 2 
+            j++;    // j=1 ; j= 2
         }
     }
-    
-    
-    if(j < s.length){ 
-        counter+= symbolValue[s[j]]; 
+
+    if(j < s.length){ // true 2 < 3
+        counter+= symbolValue[s[j]]; // counter = 2 + 1 = 3
     }
 
-    return counter;
+    return counter; // counter= 3
+    
 };
