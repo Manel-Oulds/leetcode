36a61b8a5f40cd78e87b6aae6372f1714c08eb8d
Manel-Oulds <oulds.manel@gmail.com>
2023-11-03 12:04:47 -0400

    Sudo code done

---
+++ romainNumberals.js
@@ -0,0 +1,17 @@
+/**
+ 
+ # Use an object to save the symbols as a key and the integer as a value.
+ # Initialize the counter = 0
+ # Iterate through the input and add the value of each key to the counter .
+ # if input[j] < input[j+1]
+        add input[j+1] - input [j] to counter 
+        add 2 to j;
+    else
+        add input[j] to counter
+        add 1 to j;
+
+ # return counter;
+ # time complexity O(n) space complexity O(1).
+
+
+ */
