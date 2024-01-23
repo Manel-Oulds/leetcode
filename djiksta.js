7239c9f47f92eed04a905df4962754623e9a845b
Manel-Oulds <oulds.manel@gmail.com>
2024-01-22 21:57:51 -0500

    Adding djikstra for the initial state

--- djiksta.js
+++ djiksta.js
@@ -30,6 +30,24 @@ class PriorityQueue {
         this.adjacencyList[vertex2].push({ node: vertex1, weight });
       }
 
-      
+      dijkstra(start, finish) {
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let path = []; // to return at the end
+        let smallest;
+    // Build up initial state
+    for (let vertex in this.adjacencyList) {
+        if (vertex === start) {
+          distances[vertex] = 0;
+          nodes.enqueue(vertex, 0);
+        } else {
+          distances[vertex] = Infinity;
+          nodes.enqueue(vertex, Infinity);
+        }
+        previous[vertex] = null;
+      }
+    }
+
 
 }
