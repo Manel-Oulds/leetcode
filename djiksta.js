9b75e8f3dbf065febe748ec76b80cb534ded97c4
Manel-Oulds <oulds.manel@gmail.com>
2024-01-22 21:56:14 -0500

    Addded verttex function

--- djiksta.js
+++ djiksta.js
@@ -15,3 +15,21 @@ class PriorityQueue {
         this.values.sort((a, b) => a.priority - b.priority);
       }
   }
+
+  class Graph {
+    constructor() {
+      this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+      }
+    
+      addEdge(vertex1, vertex2, weight) {
+        this.adjacencyList[vertex1].push({ node: vertex2, weight });
+        this.adjacencyList[vertex2].push({ node: vertex1, weight });
+      }
+
+      
+
+}
