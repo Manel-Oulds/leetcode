39b49d5520cb06840d3f8e711d235d98647af46c
Manel-Oulds <oulds.manel@gmail.com>
2024-01-23 22:54:51 -0500

    Decoding with file and pyramid

--- decode.py
+++ decode.py
@@ -4,9 +4,18 @@ def decode(file):
         line = f.readline()
         while line:
             n, val = line.split(" ")
-            hash_words[n] =val
+            hash_words[int(n)] =val
             line = f.readline()
     print(hash_words)
+    i = 0
+    j = 1
+    sentence = ""
+    while i < len(hash_words):
+        sentence += hash_words[i+j] + " "
+        i = j 
+        j+= 1 
+    print (sentence)
+        
     
 
 decode("fils.txt")
