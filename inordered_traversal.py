b37eb2dec49af33e5ae52ad6a8361a70e7f4e19b
Manel-Oulds <oulds.manel@gmail.com>
2024-01-27 23:05:16 -0500

    TreeNode class created

---
+++ inordered_traversal.py
@@ -0,0 +1,9 @@
+# Write a Python function called inorder_traversal that takes the root of a 
+# binary tree as input and returns a list containing the inorder traversal 
+# of the tree.
+
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
