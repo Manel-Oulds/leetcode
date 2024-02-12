03b9ef9a77901ee6cca64e1a4677f61209452066
Manel-Oulds <oulds.manel@gmail.com>
2024-02-12 16:02:43 -0500

    Looping and checking

---
+++ longest_sub_ones.js
@@ -0,0 +1,16 @@
+function longestSubarrayWithOnes(nums, k) {
+    let maxLength = 0;
+    let left = 0;
+    let maxOnesCount = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 1) {
+            maxOnesCount++;
+        }
+
+       
+    }
+
+    return maxLength;
+}
+
