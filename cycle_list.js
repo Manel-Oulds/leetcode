7c0c864f0546e7b8128fb046a50ea521cc45d57e
Manel-Oulds <oulds.manel@gmail.com>
2024-02-14 12:24:05 -0500

    HasCycle

--- cycle_list.js
+++ cycle_list.js
@@ -5,4 +5,21 @@ class ListNode {
     }
 }
 
+function hasCycle(head) {
+    
+
+    let slow = head;
+    let fast = head.next;
+
+    while (fast !== slow) {
+        if (!fast || !fast.next) {
+            return false; // Reached end of list without cycle
+        }
+        slow = slow.next; // Move slow pointer by one step
+        fast = fast.next.next; // Move fast pointer by two steps
+    }
+
+    return true; // If fast and slow meet, there is a cycle
+}
+
 
