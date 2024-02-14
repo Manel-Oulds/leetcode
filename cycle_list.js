3f485f83ebc3f92e042d6f4fa82ec76662f3cfc3
Manel-Oulds <oulds.manel@gmail.com>
2024-02-14 12:24:43 -0500

    Test done

--- cycle_list.js
+++ cycle_list.js
@@ -24,4 +24,16 @@ function hasCycle(head) {
     return true; // If fast and slow meet, there is a cycle
 }
 
+// Test the function
+const head = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node3; // Cycle
 
+console.log(hasCycle(head)); // Output: true
