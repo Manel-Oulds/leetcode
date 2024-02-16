74c1084d6773f3950f1473024651bc7d07c91e3d
Manel-Oulds <oulds.manel@gmail.com>
2024-02-16 12:18:09 -0500

    Fixed and done

--- add_two_linked.js
+++ add_two_linked.js
@@ -15,6 +15,10 @@ function addTwoNumbers(l1, l2) {
         const y = l2 ? l2.val : 0;
         const sum = x + y + carry;
 
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
         if (l1) l1 = l1.next;
         if (l2) l2 = l2.next;
     }
