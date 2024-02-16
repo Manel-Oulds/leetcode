44aa2fd9579427e0621cbd53c0c9e5301cff330f
Manel-Oulds <oulds.manel@gmail.com>
2024-02-16 12:02:20 -0500

    sum current and next

--- add_two_linked.js
+++ add_two_linked.js
@@ -5,5 +5,18 @@ class ListNode {
     }
 }
 
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode();
+    let current = dummyHead;
+    let carry = 0;
 
+    while (l1 !== null || l2 !== null) {
+        const x = l1 ? l1.val : 0;
+        const y = l2 ? l2.val : 0;
+        const sum = x + y + carry;
+
+       
+    }
+
+}
 
