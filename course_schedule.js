16b18edbd0010dc73e2f0b6795a99da6037a725a
Manel-Oulds <oulds.manel@gmail.com>
2024-02-18 16:12:53 -0500

    Added degree 0

--- course_schedule.js
+++ course_schedule.js
@@ -8,9 +8,14 @@ function canFinish(numCourses, prerequisites) {
         inDegree[course]++;
     }
     
+    const queue = [];
     
-    
-   
+    // Add courses with in-degree 0 to the queue
+    for (let i = 0; i < numCourses; i++) {
+        if (inDegree[i] === 0) {
+            queue.push(i);
+        }
+    }
     
   
     
