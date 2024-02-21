cf66f1d3f5507bf0b1b7c8d4a98f16d539f81a99
Manel-Oulds <oulds.manel@gmail.com>
2024-02-21 11:51:08 -0500

    DFS

--- bipartite_graph.js
+++ bipartite_graph.js
@@ -1,4 +1,18 @@
 function isBipartite(graph) {
     const colors = new Map(); // Map to store colors assigned to nodes
 
+    const dfs = (node, color) => {
+        if (colors.has(node)) {
+            return colors.get(node) === color; // Check if current color matches previously assigned color
+        }
+        colors.set(node, color); // Assign color to the current node
+
+        for (let neighbor of graph[node]) {
+            if (!dfs(neighbor, color ^ 1)) { // Color neighbor with opposite color
+                return false; // If conflict is detected, graph is not bipartite
+            }
+        }
+        return true;
+    };
+
 }
