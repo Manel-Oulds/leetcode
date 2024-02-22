7a850b8fa4969c6b2050d4e11720a2533a1f1469
Manel-Oulds <oulds.manel@gmail.com>
2024-02-22 12:39:00 -0500

    Intersection done

--- intersection_list.js
+++ intersection_list.js
@@ -4,3 +4,15 @@ class ListNode {
         this.next = next;
     }
 }
+
+function getIntersectionNode(headA, headB) {
+    let pointerA = headA;
+    let pointerB = headB;
+
+    while (pointerA !== pointerB) {
+        pointerA = pointerA ? pointerA.next : headB;
+        pointerB = pointerB ? pointerB.next : headA;
+    }
+
+    return pointerA;
+}
