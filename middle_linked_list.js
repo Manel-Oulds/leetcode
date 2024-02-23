bd2c5a11e3f5b1af63c0ba4e4e40a0fc3bdd093b
Manel-Oulds <oulds.manel@gmail.com>
2024-02-23 11:43:15 -0500

    Using 2 pointers

--- middle_linked_list.js
+++ middle_linked_list.js
@@ -4,3 +4,16 @@ class ListNode {
         this.next = next;
     }
 }
+
+function middleNode(head) {
+    let slow = head;
+    let fast = head;
+
+    // Use fast and slow pointers to find the middle node
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next; // Move slow pointer one step forward
+        fast = fast.next.next; // Move fast pointer two steps forward
+    }
+
+    return slow; // Return the middle node
+}
