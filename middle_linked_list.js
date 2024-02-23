8fbd660d6ccee6fb683153add6907884f04c9db2
Manel-Oulds <oulds.manel@gmail.com>
2024-02-23 11:44:08 -0500

    Test done

--- middle_linked_list.js
+++ middle_linked_list.js
@@ -17,3 +17,14 @@ function middleNode(head) {
 
     return slow; // Return the middle node
 }
+
+// Test the function
+// Create the linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+const middle = middleNode(head);
+console.log(middle.val); // Output: 3
