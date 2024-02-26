dad3c746f8dc296c768d4c0bb975d295d4e18a42
Manel-Oulds <oulds.manel@gmail.com>
2024-02-26 15:41:54 -0500

    Merge helper function

--- merge_sorted_list.js
+++ merge_sorted_list.js
@@ -5,7 +5,23 @@ class ListNode {
     }
 }
 
+function mergeTwoLists(l1, l2) {
+    let dummy = new ListNode(); // Dummy node to simplify handling of head
+    let current = dummy; // Pointer to the current node of the merged list
 
+    // Compare nodes from both lists and append the smaller node to the merged list
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+}
 
 
 
