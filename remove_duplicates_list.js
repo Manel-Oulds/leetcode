3fb5eeaef7417b4ac3a906976c6c764f95129a65
Manel-Oulds <oulds.manel@gmail.com>
2024-02-27 12:08:39 -0500

    Test done

--- remove_duplicates_list.js
+++ remove_duplicates_list.js
@@ -22,3 +22,19 @@ function deleteDuplicates(head) {
     return head; // Return the head of the modified list
 }
 
+// Test the function
+// Create the sorted linked list: 1 -> 1 -> 2
+const head = new ListNode(1);
+head.next = new ListNode(1);
+head.next.next = new ListNode(2);
+
+const newList = deleteDuplicates(head);
+
+// Print the modified list
+let current = newList;
+let output = [];
+while (current !== null) {
+    output.push(current.val);
+    current = current.next;
+}
+console.log(output); // Output: [1, 2]
