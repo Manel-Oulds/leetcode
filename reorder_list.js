ef07524b9e77a103ad1cf05a88b028a4420d7d7b
Manel-Oulds <oulds.manel@gmail.com>
2024-02-29 12:02:18 -0500

    Test done

--- reorder_list.js
+++ reorder_list.js
@@ -40,3 +40,20 @@ function reorderList(head) {
     }
 }
 
+// Test the function
+// Create the linked list: 1 -> 2 -> 3 -> 4
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+
+reorderList(head);
+
+// Print the reordered list
+let current = head;
+let output = [];
+while (current !== null) {
+    output.push(current.val);
+    current = current.next;
+}
+console.log(output); // Output: [1, 4, 2, 3]
