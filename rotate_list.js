b56079fe93eaf5c0b30532aab4fad60e566cd123
Manel-Oulds <oulds.manel@gmail.com>
2024-02-29 11:58:44 -0500

    Function / Length/ Case1

--- rotate_list.js
+++ rotate_list.js
@@ -5,4 +5,25 @@ class ListNode {
     }
 }
 
+function rotateRight(head, k) {
+    if (!head || k === 0) return head;
+
+    let length = 1;
+    let tail = head;
+
+    // Calculate the length of the list and find the tail node
+    while (tail.next !== null) {
+        tail = tail.next;
+        length++;
+    }
+
+    // Adjust k to be within the length of the list
+    k = k % length;
+
+   
+
+   
+   
+}
+
 
