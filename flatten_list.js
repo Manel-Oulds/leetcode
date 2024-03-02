e9fd0d97c62ad87b0a8d39db48a91c0eaf0c68a0
Manel-Oulds <oulds.manel@gmail.com>
2024-03-02 13:17:00 -0500

    ListNode Class

---
+++ flatten_list.js
@@ -0,0 +1,11 @@
+class Node {
+    constructor(val, prev = null, next = null, child = null) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+        this.child = child;
+    }
+}
+
+
+
