402a82fcb1b7dcf14855d51a23197d8c6942c2c6
Manel-Oulds <oulds.manel@gmail.com>
2024-03-04 14:23:08 -0500

    ReverseKgroup function

--- reverse_node_k.js
+++ reverse_node_k.js
@@ -5,4 +5,24 @@ class ListNode {
     }
 }
 
+function reverseKGroup(head, k) {
+    if (!head || k === 1) return head;
+
+    let dummy = new ListNode(); // Dummy node to simplify handling of head
+    dummy.next = head;
+    let prev = dummy;
+    let current = head;
+    let count = 0;
+
+    // Find the length of the list
+    let length = 0;
+    while (head !== null) {
+        length++;
+        head = head.next;
+    }
+
+  
+
+    return dummy.next; // Return the head of the modified list
+}
 
