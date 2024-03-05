7e2247f5b6a6552e81de472307783e4bf6a9e30a
Manel-Oulds <oulds.manel@gmail.com>
2024-03-05 12:15:40 -0500

    Iterating through the list and adding to the stack

--- flatten_multy_doubly_list.js
+++ flatten_multy_doubly_list.js
@@ -13,7 +13,17 @@ function flatten(head) {
     let stack = [];
     let current = head;
 
- 
+    while (current !== null || stack.length > 0) {
+        if (current.child !== null) {
+            if (current.next !== null) {
+                stack.push(current.next);
+            }
+            current.next = current.child;
+            current.next.prev = current;
+            current.child = null;
+        } 
+        current = current.next;
+    }
 
     return head;
 }
