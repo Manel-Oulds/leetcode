e0fdee5050238e6c5c9ae8c6ba64343ba0d5c549
Manel-Oulds <oulds.manel@gmail.com>
2024-03-06 12:45:51 -0500

    Iterating and adding

---
+++ longest_sub_no_rep.js
@@ -0,0 +1,16 @@
+function lengthOfLongestSubstring(s) {
+    let longestLength = 0;
+    let charMap = new Map();
+    let start = 0;
+
+    for (let end = 0; end < s.length; end++) {
+        
+            start = charMap.get(s[end]) + 1;
+      
+      
+    }
+
+
+}
+
+
