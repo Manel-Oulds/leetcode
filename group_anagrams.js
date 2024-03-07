241f77d0ee2273167f14e59b2dd204f9d05b6ba4
Manel-Oulds <oulds.manel@gmail.com>
2024-03-07 12:39:16 -0500

    Sort characters to identify anagrams

---
+++ group_anagrams.js
@@ -0,0 +1,14 @@
+function groupAnagrams(strs) {
+    let anagramsMap = new Map();
+
+    // Iterate through each string in the array
+    for (let str of strs) {
+        // Sort the characters of the string to identify anagrams
+        let sortedStr = str.split('').sort().join('');
+
+       
+    }
+
+
+}
+
