50d03fc7cc082f5ca854f01ab15a0a16cf8e29e3
Manel-Oulds <oulds.manel@gmail.com>
2024-03-08 11:47:00 -0500

    sorting ans start merge

---
+++ merge_interval.js
@@ -0,0 +1,10 @@
+function merge(intervals) {
+    if (intervals.length === 0) return [];
+
+    intervals.sort((a, b) => a[0] - b[0]); // Sort intervals based on start time
+
+    const merged = [intervals[0]];
+
+   
+}
+
