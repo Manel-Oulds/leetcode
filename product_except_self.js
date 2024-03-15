3c3e878677d3f962e27216803c2771b79974f37d
Manel-Oulds <oulds.manel@gmail.com>
2024-03-14 23:18:07 -0400

    Left side calculated

--- product_except_self.js
+++ product_except_self.js
@@ -2,10 +2,14 @@ function productExceptSelf(nums) {
     const n = nums.length;
     const output = new Array(n).fill(1);
 
-   
+    // Calculate left products
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        output[i] *= leftProduct;
+        leftProduct *= nums[i];
+    }
 
    
 
     return output;
 }
-
