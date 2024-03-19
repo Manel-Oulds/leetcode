244d3096d6635d1ab2084a63675ec3650435b766
Manel-Oulds <oulds.manel@gmail.com>
2024-03-18 21:40:42 -0400

    Remove Front/Rear

--- deque_with_stack.js
+++ deque_with_stack.js
@@ -7,7 +7,7 @@ class MyDeque {
     addFront(x){
         this.stack1.push(x);
     }
-    
+
     addRear(x){
         while(this.stack1.length>0){
             this.stack2.push(this.stack1.pop());
@@ -16,6 +16,14 @@ class MyDeque {
         while(this.stack2.length>0){
             this.stack1.push(this.stack2.pop());
         }
+    }
+
+    removeFront(){
+        return this.stack1.pop();
+    }
+    
+    removeRear(){
+        return this.stack1.shift();
     }
 
 }
