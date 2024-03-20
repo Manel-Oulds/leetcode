45155a1ef50c6d9dd830c9ef4401ac00ea77f051
Manel-Oulds <oulds.manel@gmail.com>
2024-03-20 12:12:08 -0400

    evaluateExpression

--- polish_notation.js
+++ polish_notation.js
@@ -4,4 +4,17 @@ function isOperator(token) {
     return token === '+' || token === '-' || token === '*' || token === '/';
 }
 
+function evaluateExpression(operand1, operand2, operator) {
+    switch (operator) {
+        case '+':
+            return operand1 + operand2;
+        case '-':
+            return operand1 - operand2;
+        case '*':
+            return operand1 * operand2;
+        case '/':
+            // In JavaScript, division truncates towards zero, so we need to handle negative numbers carefully
+            return operand1 / operand2 > 0 ? Math.floor(operand1 / operand2) : Math.ceil(operand1 / operand2);
+    }
+}
 
