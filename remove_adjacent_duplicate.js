4bda840c3f2ee04c65bbe8b64a8a259c3a409ceb
Manel-Oulds <oulds.manel@gmail.com>
2024-03-21 18:16:44 -0400

    Using stacks

--- remove_adjacent_duplicate.js
+++ remove_adjacent_duplicate.js
@@ -1,6 +1,5 @@
 function removeDuplicates(s) {
     const stack = [];
-
     for (const char of s) {
         if (stack.length > 0 && stack[stack.length - 1] === char) {
             stack.pop(); // Remove the top element of the stack if it's equal to the current character
