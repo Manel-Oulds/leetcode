6e96b97d22bcef9cf0b31bcadb17639b639c8827
Manel-Oulds <oulds.manel@gmail.com>
2024-03-21 18:16:01 -0400

    Case 2 added

--- remove_adjacent_duplicate.js
+++ remove_adjacent_duplicate.js
@@ -4,7 +4,9 @@ function removeDuplicates(s) {
     for (const char of s) {
         if (stack.length > 0 && stack[stack.length - 1] === char) {
             stack.pop(); // Remove the top element of the stack if it's equal to the current character
-        } 
+        } else {
+            stack.push(char); // Otherwise, push the current character onto the stack
+        }
     }
 
     return stack.join(''); // Join the characters remaining in the stack to form the final string
