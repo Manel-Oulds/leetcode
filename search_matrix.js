aa7a036911abd0af43b917d174919968b08daf50
Manel-Oulds <oulds.manel@gmail.com>
2024-03-22 14:55:13 -0400

    Test done

--- search_matrix.js
+++ search_matrix.js
@@ -17,3 +17,15 @@ function searchMatrix(matrix, target) {
     return false;
 }
 
+// Test the function
+const matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+];
+const target1 = 5;
+const target2 = 20;
+console.log(searchMatrix(matrix, target1)); // Output: true
+console.log(searchMatrix(matrix, target2)); // Output: false
