a60754300809b70d47af4127f42aafe536bbec71
Manel-Oulds <oulds.manel@gmail.com>
2024-03-23 23:51:28 -0400

    Condition added

--- Maximal_quare.js
+++ Maximal_quare.js
@@ -8,14 +8,14 @@ function maximalSquare(matrix) {
 
     for (let i = 1; i <= rows; i++) {
         for (let j = 1; j <= cols; j++) {
-            
+            if (matrix[i - 1][j - 1] === '1') {
                 dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
                 maxSide = Math.max(maxSide, dp[i][j]);
-            
+            }
         }
     }
 
-
+    return maxSide * maxSide;
 }
 
 
