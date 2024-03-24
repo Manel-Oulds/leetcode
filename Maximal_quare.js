e656c02e53fb8aa90e3a463c9dcca448d1a0de25
Manel-Oulds <oulds.manel@gmail.com>
2024-03-23 23:51:08 -0400

    Iterating and checking the max

--- Maximal_quare.js
+++ Maximal_quare.js
@@ -6,6 +6,15 @@ function maximalSquare(matrix) {
     let maxSide = 0;
     const dp = Array.from({ length: rows + 1 }, () => Array(cols + 1).fill(0));
 
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 1; j <= cols; j++) {
+            
+                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+                maxSide = Math.max(maxSide, dp[i][j]);
+            
+        }
+    }
+
 
 }
 
