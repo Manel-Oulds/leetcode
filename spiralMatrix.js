8902075c9d9dbf26da5512b7cd31573319382fb8
Manel-Oulds <oulds.manel@gmail.com>
2024-03-25 22:04:00 -0400

    Spiral order looping

---
+++ spiralMatrix.js
@@ -0,0 +1,29 @@
+function spiralOrder(matrix) {
+    if (matrix.length === 0 || matrix[0].length === 0) return [];
+
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    let result = [];
+
+    let top = 0;
+    let bottom = rows - 1;
+    let left = 0;
+    let right = cols - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse top row from left to right
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+
+
+       
+
+       
+    }
+
+    return result;
+}
+
