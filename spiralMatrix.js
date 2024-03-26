d810cbe501f6a8f658cf18e2c1f7a3e90afc2703
Manel-Oulds <oulds.manel@gmail.com>
2024-03-25 22:07:38 -0400

    Test done

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -43,3 +43,10 @@ function spiralOrder(matrix) {
     return result;
 }
 
+// Test the function
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(spiralOrder(matrix)); // Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
