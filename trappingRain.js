4f10e1bbe9b754213d19a30546fb471935f65b17
Manel-Oulds <oulds.manel@gmail.com>
2024-03-27 22:40:58 -0400

    trapping rain water function

---
+++ trappingRain.js
@@ -0,0 +1,22 @@
+function trap(height) {
+    const n = height.length;
+    let leftMax = 0;
+    let rightMax = 0;
+    let left = 0;
+    let right = n - 1;
+    let trappedWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                trappedWater += leftMax - height[left];
+            }
+            left++;
+        }
+    }
+
+    return trappedWater;
+}
+
